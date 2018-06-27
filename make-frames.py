efe1f473108e0f422de2f5b63494e9011d672fa9
Daniel Williams <daniel.williams@ligo.org>
2018-06-27 16:10:41 +0100

    Added frame creation script.

---
+++ make-frames.py
@@ -0,0 +1,8 @@
+from minke import mdctools, distribution, sources
+import numpy as np
+o1 = mdctools.HWFrameSet(['L1', 'H1', 'V1'])
+mdcset = mdctools.MDCSet(['L1', 'H1', 'V1'])
+mdcset.load_xml("sg_training.xml.gz")
+mdc_folder = "frames"
+for o1frame in o1.frames:
+   o1frame.generate_pcal(mdcset, mdc_folder, 'SCIENCE', rate=4096)
