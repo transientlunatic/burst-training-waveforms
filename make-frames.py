52a42e7b0a2e5c340b9e1424464eca6831d1296c
Daniel Williams <mail@daniel-williams.co.uk>
2018-06-29 16:57:33 +0100

    Added the updated scripts.

--- make-frames.py
+++ make-frames.py
@@ -1,8 +1,9 @@
 from minke import mdctools, distribution, sources
 import numpy as np
+
 o1 = mdctools.HWFrameSet(['L1', 'H1', 'V1'])
 mdcset = mdctools.MDCSet(['L1', 'H1', 'V1'])
 mdcset.load_xml("sg_training.xml.gz")
-mdc_folder = "frames"
+mdc_folder = "/home/daniel/testframes/"
 for o1frame in o1.frames:
    o1frame.generate_pcal(mdcset, mdc_folder, 'SCIENCE', rate=4096)
--- make-xml.py
+++ make-xml.py
@@ -2,22 +2,20 @@ from minke import mdctools, distribution, sources
 import numpy as np
 mdcset = mdctools.MDCSet(['L1', 'H1', 'V1'])
 
-times = distribution.even_time(start = 0, stop = 50000, rate = 630720, jitter = 20)
-hrss_values = distribution.log_uniform(5e-23, 1e-20, len(times))
+times = np.linspace(0, 2000, 500)
 
 qq = np.random.uniform(1,100, len(times))
 ff = np.random.uniform(300, 2000, len(times))
 
-for hrss, time, q, f in zip(hrss_values, times, qq, ff):
-   sg = sources.SineGaussian(frequency=f, q=q, hrss=hrss, time=time, polarisation="circular")
+for time, q, f in zip(times, qq, ff):
+   sg = sources.SineGaussian(frequency=f, q=q, hrss=1e-23, time=time, polarisation="circular")
    mdcset + sg
 
 mdcset.save_xml('sg_training.xml.gz')
+o1 = mdctools.HWFrameSet('frame_list.dat')
 
-# o1 = mdctools.FrameSet('frame_list.dat')
-
-# mdc_folder = "frames"
-# for o1frame in o1.frames:
-#    o1frame.generate_gwf(mdcset, mdc_folder, 'SCIENCE')
+mdc_folder = "/home/daniel/repositories/burst-training-waveforms/frames"
+for o1frame in o1.frames:
+   o1frame.generate_pcal(mdcset, mdc_folder)
 
 # o1.full_logfile(mdcset, 'frames/logfile.txt')
